b223cfb77a6eaff04ba7c5c86a1583b41253207d
cortes9 <lcortescedillo@csumb.edu>
2026-02-12 19:31:20 -0800

    first

--- src/main/java/Jotto.java
+++ src/main/java/Jotto.java
@@ -1,7 +1,58 @@
 /**
- * @author feng3302
+ * @author Luis Cortes
  * @version 0.1.0
- * @Since 1/29/26
+ * @Since 2/12/2026
  **/
 public class Jotto {
+        private int Word_SiZE;
+        private String cuttentWord;
+        private int score;
+        private String[] playGuesses;
+        private String[] playWords;
+        private String filename;
+        private String[] wordList;
+        private boolean DEBUG;
+        public boolean pickWords;
+        public String showWordList(){
+
+        };
+        public String[] showPlayerGuesses(){
+
+        };
+        public String[] void playerGuessScores(){
+
+        };
+        public void setCurrentWord(String){
+
+        }
+        public String[] redWords(){
+
+        }
+        public void play(){
+
+        }
+        public int guess(){
+
+        }
+        public String[] getPlayWords){
+
+        }
+        public String getCurrentWord(){
+
+        }
+        public  int getLeterCount(String){
+
+        }
+        public String showPLayedWords(){
+
+        }
+        public boolean addPlayerGuess(String){
+
+        }
+        public void updateWordList(){
+        }
+
+    }
+
+
 }
